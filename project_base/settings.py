48b52d8571737bba64e22af239dbc80f7433cb7e
GRMaruf <golamrasulmaruf@gmail.com>
2026-05-03 18:12:55 +0600

    toggle animation added

--- project_base/settings.py
+++ project_base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+x=v=_f9s%(lv=kb%(xyp49gug^!fc2q!kkmawgu44z%+jz#=f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
